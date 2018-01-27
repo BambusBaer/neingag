d4fdd0e342844664d810f02890315e8f0ed275ae
Bjoern Ammon <inf2517@hs-worms.de>
2018-01-27 13:22:31 +0100

    db dump

--- db/neingag.sql
+++ db/neingag.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 23. Jan 2018 um 16:46
--- Server-Version: 10.1.21-MariaDB
--- PHP-Version: 7.1.2
+-- Erstellungszeit: 25. Jan 2018 um 03:55
+-- Server-Version: 10.1.22-MariaDB
+-- PHP-Version: 7.1.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -51,7 +53,8 @@ INSERT INTO `comments` (`commentID`, `imageID`, `userID`, `comment`, `nickname`)
 (43, 3, 65, 'Test', 'Admin'),
 (44, 5, 65, 'Test', 'Admin'),
 (45, 5, 65, 'Hallo\r\n', 'Admin'),
-(57, 15, 65, 'Ha', 'Admin');
+(57, 15, 65, 'Ha', 'Admin'),
+(0, 14, 67, 'grgrg', 'fatHobbit');
 
 -- --------------------------------------------------------
 
@@ -62,31 +65,54 @@ INSERT INTO `comments` (`commentID`, `imageID`, `userID`, `comment`, `nickname`)
 CREATE TABLE `images` (
   `imageId` int(11) NOT NULL,
   `userName` varchar(255) NOT NULL,
-  `userImagenumber` int(255) NOT NULL,
+  `userImagenumber` int(100) NOT NULL,
   `datatype` varchar(10) NOT NULL,
-  `boringCounter` int(255) NOT NULL,
-  `comments` blob NOT NULL
+  `boringCounter` int(100) NOT NULL,
+  `profilPic` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `images`
 --
 
-INSERT INTO `images` (`imageId`, `userName`, `userImagenumber`, `datatype`, `boringCounter`, `comments`) VALUES
-(2, 'fatHobbit', 1, 'jpg', 3, 0x41),
-(3, 'fatHobbit', 2, 'jpg', 1, 0x41),
-(4, 'fat', 1, 'png', 5, 0x41),
-(5, 'fat', 2, 'png', 1, 0x41),
-(6, 'dick', 1, 'jpg', 8, 0x41),
-(7, 'dick', 2, 'png', 0, 0x41),
-(8, 'Admin', 1, 'png', 7, 0x41),
-(9, 'Admin', 2, 'jpg', 7, 0x41),
-(10, 'Admin', 3, 'jpg', 8, 0x41),
-(11, 'Admin', 4, 'jpg', 5, 0x41),
-(12, 'Admin', 5, 'jpg', -16, 0x41),
-(13, 'Admin', 6, 'jpg', 5, 0x41),
-(14, 'Admin', 7, 'jpg', -1, 0x41),
-(15, 'Nicky', 1, 'jpg', 1, 0x41);
+INSERT INTO `images` (`imageId`, `userName`, `userImagenumber`, `datatype`, `boringCounter`, `profilPic`) VALUES
+(2, 'fatHobbit', 1, 'jpg', 3, 0),
+(3, 'fatHobbit', 2, 'jpg', 3, 0),
+(4, 'fat', 1, 'png', 5, 0),
+(5, 'fat', 2, 'png', 1, 0),
+(6, 'dick', 1, 'jpg', 8, 0),
+(7, 'dick', 2, 'png', 1, 0),
+(8, 'Admin', 1, 'png', 7, 0),
+(9, 'Admin', 2, 'jpg', 7, 0),
+(10, 'Admin', 3, 'jpg', 9, 0),
+(11, 'Admin', 4, 'jpg', 5, 0),
+(12, 'Admin', 5, 'jpg', -15, 0),
+(13, 'Admin', 6, 'jpg', 5, 0),
+(14, 'Admin', 7, 'jpg', 1, 0),
+(15, 'Nicky', 1, 'jpg', 1, 0),
+(44, 'fatHobbit', 3, 'png', 1, 0),
+(45, 'fatHobbit', 4, 'png', 1, 0),
+(46, 'fatHobbit', 5, 'png', 1, 0),
+(47, 'fatHobbit', 6, 'png', 1, 0),
+(48, 'fatHobbit', 7, 'png', 1, 0),
+(49, 'fatHobbit', 8, 'png', 1, 0),
+(50, 'fatHobbit', 9, 'png', 1, 0),
+(51, 'fatHobbit', 10, 'png', 1, 0),
+(52, 'fatHobbit', 11, 'png', 1, 0),
+(53, 'fatHobbit', 12, 'jpg', 1, 0),
+(54, 'fatHobbit', 13, 'jpg', 1, 0),
+(55, 'fatHobbit', 14, 'jpg', 1, 0),
+(56, 'fatHobbit', 15, 'png', 1, 0),
+(57, 'fatHobbit', 16, 'jpg', 1, 0),
+(58, 'fatHobbit', 17, 'jpg', 1, 0),
+(59, 'fatHobbit', 18, 'png', 1, 0),
+(60, 'fatHobbit', 19, 'png', 1, 0),
+(61, 'fatHobbit', 20, 'jpg', 1, 0),
+(62, 'fatHobbit', 21, 'png', 1, 0),
+(63, 'fatHobbit', 22, 'jpg', 1, 0),
+(64, 'fatHobbit', 23, 'jpg', 1, 0),
+(65, 'fatHobbit', 24, 'jpg', 1, 1),
+(66, 'fatHobbit', 25, 'jpg', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -111,17 +137,11 @@ INSERT INTO `users` (`id`, `nickname`, `email`, `password`) VALUES
 (65, 'Admin', 'ad@mi.n', '$2y$10$jUl2LJzFR46qplbcsl27MOhV1OG2xPTdhI74hPNq/PvYT7n2HfDCi'),
 (66, 'Nicky', 'n@s.de', '$2y$10$KhD66NrzWIsZaKsleWwF1.ab/qWGixuqlynzatZovl16wDWcsz4Qy'),
 (67, 'fatHobbit', 'bjoern.ammon@gmx.de', '$2y$10$L2amn1nb/jYTFTzKqLL8yuImptRaywlbeOP5ty9cGb4hO.6IGtuCq'),
-(68, 'Mo', 'mo@v.de', '$2y$10$uxL5krBFQgx.RNjs349OKeTzUzr3BUdR8dtyHMEX9OCo1dC3hCxyq');
+(68, 'kaka', 'kaka@ka.ka', '$2y$10$Seh9IXHuQ98.Zy2xePrh2uJLdvsK0eHYy0ySXqhTcp3nrMDkj5sAS');
 
 --
 -- Indizes der exportierten Tabellen
 --
-
---
--- Indizes für die Tabelle `comments`
---
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`commentID`);
 
 --
 -- Indizes für die Tabelle `images`
@@ -141,20 +161,16 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT für Tabelle `comments`
---
-ALTER TABLE `comments`
-  MODIFY `commentID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
---
 -- AUTO_INCREMENT für Tabelle `images`
 --
 ALTER TABLE `images`
-  MODIFY `imageId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `imageId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 --
 -- AUTO_INCREMENT für Tabelle `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
