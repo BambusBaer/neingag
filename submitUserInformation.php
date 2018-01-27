fded490a0dbe018429bdb893171f82e713896947
Moritz Vetter <inf2557@hs-worms.de>
2018-01-27 19:47:32 +0100

    refactoring & bugfixes

---
+++ submitUserInformation.php
@@ -0,0 +1,19 @@
+<?php 
+
+    session_start(); 
+    $pdo = new PDO('mysql:host=localhost;dbname=neinGag', 'root', '');
+	
+	if(isset($_POST['firstName']) && !empty($_POST['firstName']))
+		if(isset($_POST['lastName']) && !empty($_POST['lastName']))
+			if(isset($_POST['age']) && !empty($_POST['age'])){
+				echo $_SESSION['userid'];
+				$firstName = $_POST['firstName'];
+				$lastName = $_POST['lastName'];
+				$age = $_POST['age'];
+				$statement = $pdo->prepare("UPDATE users SET firstName = ?, lastName = ?, age = ? WHERE nickname = ?");
+				$statement->execute(array($firstName, $lastName, $age, $_SESSION['userid']));
+			}
+	
+//	header('Location:'.$_SERVER['HTTP_REFERER']);
+
+?>
